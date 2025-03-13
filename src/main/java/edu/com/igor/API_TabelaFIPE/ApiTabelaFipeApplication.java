a177c1f360c7998644667c9abcd3272c78dd4392
igorhgds <igorhenrique.gomes@hotmail.com>
2025-03-13 08:49:28 -0300

    first commit

---
+++ src/main/java/edu/com/igor/API_TabelaFIPE/ApiTabelaFipeApplication.java
@@ -0,0 +1,15 @@
+package edu.com.igor.API_TabelaFIPE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTabelaFipeApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTabelaFipeApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/edu/com/igor/API_TabelaFIPE/ApiTabelaFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.com.igor.API_TabelaFIPE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTabelaFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
